3851671948f4d9cad0a6f4039f0c96958033c135
USMCFighter94 <verlander94@gmail.com>
2019-03-27 22:31:23 -0400

    Add more material fields to RSVP form

--- public/scripts/rsvp.js
+++ public/scripts/rsvp.js
@@ -1,6 +1,12 @@
 mdc.textField.MDCTextField.attachTo(document.querySelector('.name'))
-mdc.textField.MDCTextField.attachTo(document.querySelector('.username'))
-mdc.textField.MDCTextField.attachTo(document.querySelector('.password'))
+mdc.textField.MDCTextField.attachTo(document.querySelector('.party-num'))
 
 mdc.ripple.MDCRipple.attachTo(document.querySelector('.cancel'))
 mdc.ripple.MDCRipple.attachTo(document.querySelector('.next'))
+
+mdc.form-field.MDCFormField.attachTo(document.querySelector('.mdc-form-field'))
+mdc.checkbox.MDCCheckbox.attachTo(document.querySelector('.mdc-checkbox'))
+mdc.radio.MDCRadio.attachTo(document.querySelector('.mdc-radio'))
+
+formField.input = radio
+formField.input = checkbox
